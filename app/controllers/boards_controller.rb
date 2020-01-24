f0176be6dc4981d6430e58c795cedbb9c680eeda
MasakiTanaka-9627 <masa.08279@gmail.com>
2020-01-24 20:20:44 +0900

    掲示板削除途中

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,30 @@
 class BoardsController < ApplicationController
+
+    def new
+        @board = Board.new
+    end
+
+def create        
+    @board = Board.new(board_params)
+    @board.user_id =  @current_user.id
+
+    if @board.save
+        redirect_to @current_user
+    else
+        render action: :new        
+    end
+end 
+
+def destory
+    @board = Board.find_by(id: board_params[:id])
+    @board.destroy
+    redirect_to("/")
+end
+
+    private
+	 
+    def board_params
+      params.require(:board).permit(:title, :content)
+        end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
 
   get '/signup',  to:'users#new'
-
   get '/users', to: 'users#new'
+
   resources :users, only: [:index, :show, :create, :edit, :update]  
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  get '/boards', to: 'boards#new'
+  post '/boards', to: 'boards#create'
+  post "posts/:id/destroy" => "posts#destroy"
+
 end
 
