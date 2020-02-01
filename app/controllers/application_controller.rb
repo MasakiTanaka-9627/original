a3f71ebdba3d5b1134fe0e08ef76b39205c89aa0
MasakiTanaka-9627 <masa.08279@gmail.com>
2020-02-01 13:52:50 +0900

    コード修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  # どこのページからリクエストが来たか保存しておく
-  def set_request_from
-    if session[:request_from]
-      @request_from = session[:request_from]
-    end
-    # 現在のURLを保存しておく
-    session[:request_from] = request.original_url
-  end
-
-  # 前の画面に戻る
-  def return_back
-    if request.referer
-      redirect_to :back and return true
-    elsif @request_from
-      redirect_to @request_from and return true
-    end
-  end
-
   before_action :set_current_user
 
   protect_from_forgery with: :exception
