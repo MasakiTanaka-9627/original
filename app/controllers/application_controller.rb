ae5b8de259f34d142b78dce81c1ddde13fd3394a
MasakiTanaka-9627 <masa.08279@gmail.com>
2020-02-12 20:15:02 +0900

    スクレイピングコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,28 +16,28 @@ class ApplicationController < ActionController::Base
   helper_method :scraping_ios
 
   def scraping_android
-    sleep(1)
-    RecommendGame.delete_all
-    agent = Mechanize.new
-    page = agent.get("https://gamewith.jp/gamedb/android/ranking/index")
-    @elements = page.search(".gdb-ranking-box_title_text")
-    @images = page.search(".gdb-ranking-box_img img")
-    num = @elements.length
-    num.times do |i|
-      RecommendGame.create(id: i, title: @elements[i].text, image: @images[i][:src])
-    end
+    # sleep(1)
+    # RecommendGame.delete_all
+    # agent = Mechanize.new
+    # page = agent.get("https://gamewith.jp/gamedb/android/ranking/index")
+    # @elements = page.search(".gdb-ranking-box_title_text")
+    # @images = page.search(".gdb-ranking-box_img img")
+    # num = @elements.length
+    # num.times do |i|
+    #   RecommendGame.create(id: i, title: @elements[i].text, image: @images[i][:src])
+    # end
   end
 
   def scraping_ios
-    sleep(1)
-    RecommendGame.delete_all
-    agent = Mechanize.new
-    page = agent.get("https://gamewith.jp/gamedb/ios/ranking/index")
-    @elements = page.search(".gdb-ranking-box_title_text")
-    @images = page.search(".gdb-ranking-box_img img")
-    num = @elements.length
-    num.times do |i|
-      RecommendGame.create!(id: i, title: @elements[i].text, image: @images[i][:src])
-    end
-  end
+  #   sleep(1)
+  #   RecommendGame.delete_all
+  #   agent = Mechanize.new
+  #   page = agent.get("https://gamewith.jp/gamedb/ios/ranking/index")
+  #   @elements = page.search(".gdb-ranking-box_title_text")
+  #   @images = page.search(".gdb-ranking-box_img img")
+  #   num = @elements.length
+  #   num.times do |i|
+  #     RecommendGame.create!(id: i, title: @elements[i].text, image: @images[i][:src])
+  #   end
+   end
 end
